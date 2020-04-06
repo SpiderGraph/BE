1e525070d13b2bea15184d8ac63f57dad9be8e3f
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-04-06 14:38:20 -0700

    configured

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const helmet = require('helmet')
+// DB
+const mongoose = require('mongoose')
+// import Routes
+const graphRoute = require('../routers/graphs/graphsRouter')
+
+// Apply middleware
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+// Apply routers
+server.use('/graphs', graphRoute)
+
+server.get('/', (req, res) => {
+    res.status(200).json('Heyhey')
+})
+
+// Connect to db
+ mongoose.connect(process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+   () => {
+     console.log('DB connected')
+ })
+
+module.exports = server
+
