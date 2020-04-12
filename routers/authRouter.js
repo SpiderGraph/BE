351aec8b3e0e86a39e1b247fa9dc1bd18d9ba082
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-04-11 17:46:00 -0700

    console data

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -17,8 +17,11 @@ router.post('/register', validateUser, hashPassword, (req, res) => {
 router.post('/login', (req, res) => {
     const {username, password} = req.body
     if(username && password){
+        console.log('CREDENTIALS ', req.body)
         Users.findOne().where({username: username})
         .then(data => {
+            console.log('DATA ', data)
+            console.log('BCRYPT ', bcrypt.compareSync(password, data.password))
             if(data && bcrypt.compareSync(password, data.password)){
                 const token = generateToken(data)
                 res.status(200).json(token)
